d4f14b3c55c8d0991876207b3c0a5163123dea47
Laney Stroup <laney@stroups.net>
2021-12-18 07:10:24 -0600

    cleanup complexity in to_ordinal method;

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -81,22 +81,29 @@ module TwelveDaysOfChristmas
     end
 
     def to_ordinal
-      @number.to_s + (
-        case @number
-        when 1
-          "st"
-        when 2
-          "nd"
-        when 3
-          "rd"
-        else
-          "th"
-        end
-      )
+      @number.to_s + ordinal_ending
     end
 
     def to_article
       @number == 1 ? "a" : @number
+    end
+
+    private
+
+    def ordinal_ending
+      self.class.custom_ordinal_endings[@number] || self.class.default_ordinal_ending
+    end
+
+    def self.custom_ordinal_endings
+      {
+        1 => "st",
+        2 => "nd",
+        3 => "rd"
+      }
+    end
+
+    def self.default_ordinal_ending
+      "th"
     end
 
   end
